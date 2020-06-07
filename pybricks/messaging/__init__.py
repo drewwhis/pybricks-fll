680c6ac4e04dae80998c8c5711edb9542bd687b3
Lawrence (Drew) Whisenant <7390131+drewwhis@users.noreply.github.com>
2020-06-06 21:32:00 -0500

    order imports property in __init__ files

--- pybricks/messaging/__init__.py
+++ pybricks/messaging/__init__.py
@@ -3,7 +3,7 @@ __all__ = ["BluetoothMailboxClient", "BluetoothMailboxServer",
 
 from .__stub.__bluetoothmailboxclient import BluetoothMailboxClient
 from .__stub.__bluetoothmailboxserver import BluetoothMailboxServer
-from .__stub.__logicmailbox import LogicMailbox
 from .__stub.__mailbox import Mailbox
+from .__stub.__logicmailbox import LogicMailbox
 from .__stub.__numericmailbox import NumericMailbox
 from .__stub.__textmailbox import TextMailbox
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybricks-stubs",
-    version="1.0.7",
+    version="1.0.8",
     author="Lawrence (Drew) Whisenant",
     author_email="dwhisenant@firstpartners.org",
     description="A stubs package to write EV3 Python code.",
